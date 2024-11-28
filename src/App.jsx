78927ab82c7569974f5b2031b90f13c8743f39b7
TALITA ANTUNES <127903101+Talitaav@users.noreply.github.com>
2024-11-28 19:57:27 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import RelogioDigital from './components/RelogioDigital'
+
+function App() {
+
+  return (
+    <>
+    <h1>Olá Mundo!</h1>
+    <RelogioDigital/>
+    </>
+  )
+}   
+
+export default App
